c48d006df90d1e56e8fec51d670af38fa8654e0a
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-03-11 10:45:14 +0530

    Add files via upload

---
+++ Assignment 6/assignment6.cpp
@@ -0,0 +1,8 @@
+/*
+Assignment 6: 
+Represent a given graph using adjacency list to perform DFS and BFS.
+Use the map of the area around the college as the graph. 
+Identify the prominent landmarks as nodes and perform DFS and BFS on that.
+
+
+*/
