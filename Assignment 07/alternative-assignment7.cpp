77189bf2da4a4f5e0d81755b336696f9b44083b9
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-05-12 22:24:39 +0530

    Create alternative-assignment7.cpp

---
+++ Assignment 07/alternative-assignment7.cpp
@@ -0,0 +1,13 @@
+/*
+Tour operator organizes guided bus trips across the Maharashtra.
+Tourists may have different preferences. Tour operator offers a choice
+from many different routes. Every day the bus moves from starting city
+S to another city F as chosen by client. On this way, the tourists can see
+the sights alongside the route travelled from S to F. Client may have
+preference to choose route. There is a restriction on the routes that the
+tourists may choose from, the bus has to take a short route from S to F
+or a route having one distance unit longer than the minimal distance.
+Two routes from S to F are considered different if there is at least one
+road from a city A to a city B which is part of one route, but not of the
+other route
+*/
